daaf44e89ffc1d8829cdd65716900164ba0fdc91
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-07-31 15:06:45 +0900

    server

---
+++ server/src/index.ts
@@ -0,0 +1,48 @@
+import { ApolloServer, gql } from "apollo-server";
+import * as fs from "fs";
+import { Query, Resolvers } from "./generated/graphql";
+
+const typeDefs = gql`
+  ${fs.readFileSync(__dirname.concat("/../schema.gql"), "utf8")}
+`;
+
+interface LoadingDataContext {
+  Query: Query;
+}
+
+const resolvers: Resolvers<LoadingDataContext> = {
+  Query: {
+    hello: async (parent, args, context, info) => {
+      return context.Query.hello;
+    },
+  },
+};
+
+const readJsonFile = async (relativeFileName: string): Promise<any> => {
+  const jsonDataFile = __dirname.concat(relativeFileName);
+  const fileContent = await fs.promises.readFile(jsonDataFile, "utf8");
+  const jsonData = JSON.parse(fileContent);
+  return jsonData;
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }: any) => {
+    try {
+      const queryData: LoadingDataContext = await readJsonFile(
+        "/../data/Query.json"
+      );
+      return { Query: queryData };
+    } catch (err) {
+      console.log("***ERROR OCURRED***");
+      console.log(err);
+      throw new Error("internal error happened!!");
+    }
+  },
+});
+
+// The  method launches a web server.
+server.listen().then(({ url }) => {
+  console.log();
+});
--- steps/1-setup-apollo-server.sh
+++ steps/1-setup-apollo-server.sh
@@ -8,6 +8,7 @@ mkdir server
 # shellcheck disable=SC2164 # REMOVE THIS IN aggregate.sh
 cd server
 
+# node.js setup
 npm init -y
 echo "node_modules" > .gitignore
 
@@ -15,33 +16,30 @@ echo "node_modules" > .gitignore
 npm install --save-dev typescript
 npx tsc --init
 
-# ts-node-dev for watch and restart a TypeScript server
+# ts-node-dev: watch and restart a TypeScript server
 npm install --save-dev ts-node-dev
 npm set-script start "ts-node-dev --respawn src/index.ts"
 
 # apollo server
 npm install apollo-server graphql
 
-
-# ## Set up Codegen
 # install and setup graphql-codegen
 npm install -D @graphql-codegen/cli # @2.10.0
-# 本来ならここで npx graphql-code-generator init
-    # ? What type of application are you building? Backend - API or server
-    # ? Where is your schema?: (path or url) schema.gql
-    # ? Pick plugins: TypeScript (required by other typescript plugins),
-    # TypeScript Resolvers (strongly typed resolve functions)
-    # ? Where to write the output: src/generated/graphql.ts
-    # ? Do you want to generate an introspection file? No
-    # ? How to name the config file? codegen.yml
-    # ? What script in package.json should run the codegen? generate
+# 本来ならここで npx graphql-code-generator initを行うが、対話モードに入ると時間がかかるので以下npm installで代替
 npm install --save-dev  @graphql-codegen/typescript @graphql-codegen/typescript-resolvers
 npm set-script generate "graphql-codegen --config codegen.yml --watch ./schema.gql" # update generate script
 
-curl https://raw.githubusercontent.com/richardimaoka/tutorial-apollo-server-setup/main/server/config.yml > config.yml
-curl https://raw.githubusercontent.com/richardimaoka/tutorial-apollo-server-setup/main/server/schema.gql > schema.gql
+
+# copy files
 mkdir src
-curl https://raw.githubusercontent.com/richardimaoka/tutorial-apollo-server-setup/main/server/src/index.ts > src/index.ts
 mkdir data
+curl https://raw.githubusercontent.com/richardimaoka/tutorial-apollo-server-setup/main/server/codegen.yml > codegen.yml
+curl https://raw.githubusercontent.com/richardimaoka/tutorial-apollo-server-setup/main/server/schema.gql > schema.gql
+curl https://raw.githubusercontent.com/richardimaoka/tutorial-apollo-server-setup/main/server/src/index.ts > src/index.ts
 curl https://raw.githubusercontent.com/richardimaoka/tutorial-apollo-server-setup/main/server/data/Query.json > data/Query.json
 
+npm run generate
+
+# shellcheck disable=SC2164 # REMOVE THIS IN aggregate.sh
+cd server
+npm start
