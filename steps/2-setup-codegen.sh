a8e59c94733723c7fccfb65122a8909c67b6bcb0
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-08-04 05:12:03 +0900

    move source code to answer

--- steps/2-setup-codegen.sh
+++ steps/2-setup-codegen.sh
@@ -3,7 +3,96 @@
 cd "$(dirname "$0")" || exit # REMOVE THIS IN aggregate.sh
 cd ../ || exit               # REMOVE THIS IN aggregate.sh - cd to the git repository root
 
-# npm install graphql-scalars
+# :large_orange_diamond: Action: 新しいターミナルを立ち上げてください。
+
+# ```:terminal
+npm install graphql-scalars
+# ```
+
+# :large_orange_diamond: Action: 以下のとおり `server/schema.gql` を更新してください。
+
+# ```graphql:server/schema.gql
+# scalar EmailAddress
+#
+# type Person {
+#   emailAddress: EmailAddress
+#   name: String
+# }
+#
+# type Query {
+#   me: Person
+# }
+# ```
+
+# :large_orange_diamond: Action: 以下のとおり `server/src/index.ts` を更新してください。
+# ```ts:server/src/index.ts
+# import { ApolloServer, gql } from "apollo-server";
+# import * as fs from "fs";
+# import {
+#   EmailAddressResolver,
+#   TimestampResolver,
+#   URLResolver,
+# } from "graphql-scalars";
+# import { EmailAddress } from "graphql-scalars/mocks";
+# import { Query, Resolvers } from "./generated/graphql";
+#
+# const typeDefs = gql`
+#   ${fs.readFileSync(__dirname.concat("/../schema.gql"), "utf8")}
+# `;
+#
+# interface LoadingDataContext {
+#   Query: Query;
+# }
+#
+# const resolvers: Resolvers<LoadingDataContext> = {
+#   Query: {
+#     me(parent, args, context, info) {
+#       return null;
+#     },
+#   },
+#   Person: {
+#     name(parent, args, context, info) {
+#       return parent.name;
+#     },
+#     emailAddress(parent, args, context, info) {
+#       return parent.emailAddress;
+#     },
+#   },
+#   EmailAddress: EmailAddressResolver,
+# };
+#
+# const readJsonFile = async (relativeFileName: string): Promise<any> => {
+#   const jsonDataFile = __dirname.concat(relativeFileName);
+#   const fileContent = await fs.promises.readFile(jsonDataFile, "utf8");
+#   const jsonData = JSON.parse(fileContent);
+#   return jsonData;
+# };
+#
+# const server = new ApolloServer({
+#   typeDefs,
+#   resolvers,
+#   context: async ({ req }: any) => {
+#     try {
+#       const queryData: LoadingDataContext = await readJsonFile(
+#         "/../data/Query.json"
+#       );
+#       return { Query: queryData };
+#     } catch (err) {
+#       console.log("***ERROR OCURRED***");
+#       console.log(err);
+#       throw new Error("internal error happened!!");
+#     }
+#   },
+# });
+#
+# // The  method launches a web server.
+# server.listen().then(({ url }) => {
+#   console.log();
+# });
+#```
+
+# :large_orange_diamond: Action: 以下のとおり `server/data/Query.json` を更新してください。
+
 # EmailAddress - schema.gql, data/Query.json, Apollo Studio
 #   override in index.ts, and return 10 -> error https://github.com/richardimaoka/tutorial-graphql-scalars/commit/5edea8ed55cf85d145edfc3c36746a74378aa148
 #   config.yml to set string -> static type checking! https://github.com/richardimaoka/tutorial-graphql-scalars/commit/5edea8ed55cf85d145edfc3c36746a74378aa148
