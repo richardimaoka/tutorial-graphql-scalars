c348efdc2d3375d0c05114c5938b9b351cabde7e
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-08-06 09:03:58 +0900

    return 10 in EmailAddress

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ const resolvers: Resolvers<LoadingDataContext> = {
       return parent.name;
     },
     emailAddress(parent, _args, _context, _info) {
-      return parent.emailAddress;
+      return 10;
     },
   },
   EmailAddress: EmailAddressResolver,
