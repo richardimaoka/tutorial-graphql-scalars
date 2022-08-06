15071a873ad5d8d710ea3ccf34ba97c49aefca64
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-08-06 18:27:39 +0900

    Revert "many custom scalars"
    This reverts commit 4b74b088c7cbf75f5ce1500205a1952455bb4e6a.

--- server/src/generated/graphql.ts
+++ server/src/generated/graphql.ts
@@ -21,29 +21,13 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
-  DateTime: any;
   EmailAddress: string;
-  NonNegativeFloat: any;
-  NonNegativeInt: any;
-  PhoneNumber: any;
-  PositiveFloat: any;
-  PositiveInt: any;
-  PostalCode: any;
-  URL: any;
 };
 
 export type Person = {
   __typename?: "Person";
-  ageInYears: Maybe<Scalars["PositiveInt"]>;
-  birthDate: Maybe<Scalars["DateTime"]>;
-  currentlyActiveProjects: Maybe<Scalars["NonNegativeInt"]>;
   emailAddress: Maybe<Scalars["EmailAddress"]>;
-  heightInInches: Maybe<Scalars["PositiveFloat"]>;
-  homePage: Maybe<Scalars["URL"]>;
-  homePostalCode: Maybe<Scalars["PostalCode"]>;
-  minimumHourlyRate: Maybe<Scalars["NonNegativeFloat"]>;
   name: Maybe<Scalars["String"]>;
-  phoneNumber: Maybe<Scalars["PhoneNumber"]>;
 };
 
 export type Query = {
@@ -159,125 +143,38 @@ export type DirectiveResolverFn<
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = {
   Boolean: ResolverTypeWrapper<Scalars["Boolean"]>;
-  DateTime: ResolverTypeWrapper<Scalars["DateTime"]>;
   EmailAddress: ResolverTypeWrapper<Scalars["EmailAddress"]>;
-  NonNegativeFloat: ResolverTypeWrapper<Scalars["NonNegativeFloat"]>;
-  NonNegativeInt: ResolverTypeWrapper<Scalars["NonNegativeInt"]>;
   Person: ResolverTypeWrapper<Person>;
-  PhoneNumber: ResolverTypeWrapper<Scalars["PhoneNumber"]>;
-  PositiveFloat: ResolverTypeWrapper<Scalars["PositiveFloat"]>;
-  PositiveInt: ResolverTypeWrapper<Scalars["PositiveInt"]>;
-  PostalCode: ResolverTypeWrapper<Scalars["PostalCode"]>;
   Query: ResolverTypeWrapper<{}>;
   String: ResolverTypeWrapper<Scalars["String"]>;
-  URL: ResolverTypeWrapper<Scalars["URL"]>;
 };
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = {
   Boolean: Scalars["Boolean"];
-  DateTime: Scalars["DateTime"];
   EmailAddress: Scalars["EmailAddress"];
-  NonNegativeFloat: Scalars["NonNegativeFloat"];
-  NonNegativeInt: Scalars["NonNegativeInt"];
   Person: Person;
-  PhoneNumber: Scalars["PhoneNumber"];
-  PositiveFloat: Scalars["PositiveFloat"];
-  PositiveInt: Scalars["PositiveInt"];
-  PostalCode: Scalars["PostalCode"];
   Query: {};
   String: Scalars["String"];
-  URL: Scalars["URL"];
 };
-
-export interface DateTimeScalarConfig
-  extends GraphQLScalarTypeConfig<ResolversTypes["DateTime"], any> {
-  name: "DateTime";
-}
 
 export interface EmailAddressScalarConfig
   extends GraphQLScalarTypeConfig<ResolversTypes["EmailAddress"], any> {
   name: "EmailAddress";
 }
 
-export interface NonNegativeFloatScalarConfig
-  extends GraphQLScalarTypeConfig<ResolversTypes["NonNegativeFloat"], any> {
-  name: "NonNegativeFloat";
-}
-
-export interface NonNegativeIntScalarConfig
-  extends GraphQLScalarTypeConfig<ResolversTypes["NonNegativeInt"], any> {
-  name: "NonNegativeInt";
-}
-
 export type PersonResolvers<
   ContextType = any,
   ParentType extends ResolversParentTypes["Person"] = ResolversParentTypes["Person"]
 > = {
-  ageInYears: Resolver<
-    Maybe<ResolversTypes["PositiveInt"]>,
-    ParentType,
-    ContextType
-  >;
-  birthDate: Resolver<
-    Maybe<ResolversTypes["DateTime"]>,
-    ParentType,
-    ContextType
-  >;
-  currentlyActiveProjects: Resolver<
-    Maybe<ResolversTypes["NonNegativeInt"]>,
-    ParentType,
-    ContextType
-  >;
   emailAddress: Resolver<
     Maybe<ResolversTypes["EmailAddress"]>,
     ParentType,
     ContextType
   >;
-  heightInInches: Resolver<
-    Maybe<ResolversTypes["PositiveFloat"]>,
-    ParentType,
-    ContextType
-  >;
-  homePage: Resolver<Maybe<ResolversTypes["URL"]>, ParentType, ContextType>;
-  homePostalCode: Resolver<
-    Maybe<ResolversTypes["PostalCode"]>,
-    ParentType,
-    ContextType
-  >;
-  minimumHourlyRate: Resolver<
-    Maybe<ResolversTypes["NonNegativeFloat"]>,
-    ParentType,
-    ContextType
-  >;
   name: Resolver<Maybe<ResolversTypes["String"]>, ParentType, ContextType>;
-  phoneNumber: Resolver<
-    Maybe<ResolversTypes["PhoneNumber"]>,
-    ParentType,
-    ContextType
-  >;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 };
-
-export interface PhoneNumberScalarConfig
-  extends GraphQLScalarTypeConfig<ResolversTypes["PhoneNumber"], any> {
-  name: "PhoneNumber";
-}
-
-export interface PositiveFloatScalarConfig
-  extends GraphQLScalarTypeConfig<ResolversTypes["PositiveFloat"], any> {
-  name: "PositiveFloat";
-}
-
-export interface PositiveIntScalarConfig
-  extends GraphQLScalarTypeConfig<ResolversTypes["PositiveInt"], any> {
-  name: "PositiveInt";
-}
-
-export interface PostalCodeScalarConfig
-  extends GraphQLScalarTypeConfig<ResolversTypes["PostalCode"], any> {
-  name: "PostalCode";
-}
 
 export type QueryResolvers<
   ContextType = any,
@@ -286,21 +183,8 @@ export type QueryResolvers<
   me: Resolver<Maybe<ResolversTypes["Person"]>, ParentType, ContextType>;
 };
 
-export interface UrlScalarConfig
-  extends GraphQLScalarTypeConfig<ResolversTypes["URL"], any> {
-  name: "URL";
-}
-
 export type Resolvers<ContextType = any> = {
-  DateTime: GraphQLScalarType;
   EmailAddress: GraphQLScalarType;
-  NonNegativeFloat: GraphQLScalarType;
-  NonNegativeInt: GraphQLScalarType;
   Person: PersonResolvers<ContextType>;
-  PhoneNumber: GraphQLScalarType;
-  PositiveFloat: GraphQLScalarType;
-  PositiveInt: GraphQLScalarType;
-  PostalCode: GraphQLScalarType;
   Query: QueryResolvers<ContextType>;
-  URL: GraphQLScalarType;
 };
