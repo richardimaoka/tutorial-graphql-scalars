59c46fbcef5646cf739a6f5995e0a654da1e54e7
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-08-06 18:25:03 +0900

    many custom scalars

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,16 @@
 import { ApolloServer, gql } from "apollo-server";
 import * as fs from "fs";
-import { EmailAddressResolver } from "graphql-scalars";
+import {
+  DateTimeResolver,
+  EmailAddressResolver,
+  NonNegativeFloatResolver,
+  NonNegativeIntResolver,
+  PhoneNumberResolver,
+  PositiveFloatResolver,
+  PositiveIntResolver,
+  PostalCodeResolver,
+  URLResolver,
+} from "graphql-scalars";
 import { Query, Resolvers } from "./generated/graphql";
 
 const typeDefs = gql`
@@ -21,11 +31,43 @@ const resolvers: Resolvers<LoadingDataContext> = {
     name(parent, _args, _context, _info) {
       return parent.name;
     },
+    birthDate(parent, _args, _context, _info) {
+      return parent.birthDate;
+    },
+    ageInYears(parent, _args, _context, _info) {
+      return parent.ageInYears;
+    },
+    heightInInches(parent, _args, _context, _info) {
+      return parent.heightInInches;
+    },
+    minimumHourlyRate(parent, _args, _context, _info) {
+      return parent.minimumHourlyRate;
+    },
+    currentlyActiveProjects(parent, _args, _context, _info) {
+      return parent.currentlyActiveProjects;
+    },
     emailAddress(parent, _args, _context, _info) {
       return parent.emailAddress;
     },
+    homePage(parent, _args, _context, _info) {
+      return parent.homePage;
+    },
+    phoneNumber(parent, _args, _context, _info) {
+      return parent.phoneNumber;
+    },
+    homePostalCode(parent, _args, _context, _info) {
+      return parent.homePostalCode;
+    },
   },
+  DateTime: DateTimeResolver,
+  PositiveInt: PositiveIntResolver,
+  PositiveFloat: PositiveFloatResolver,
+  NonNegativeFloat: NonNegativeFloatResolver,
+  NonNegativeInt: NonNegativeIntResolver,
   EmailAddress: EmailAddressResolver,
+  URL: URLResolver,
+  PhoneNumber: PhoneNumberResolver,
+  PostalCode: PostalCodeResolver,
 };
 
 const readJsonFile = async (relativeFileName: string): Promise<any> => {
