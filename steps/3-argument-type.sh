914e785ba27b3f172e64aae8120a597e9b0fde53
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-08-06 22:04:38 +0900

    update step 2

--- steps/3-argument-type.sh
+++ steps/3-argument-type.sh
@@ -3,6 +3,8 @@
 cd "$(dirname "$0")" || exit # REMOVE THIS IN aggregate.sh
 cd ../ || exit               # REMOVE THIS IN aggregate.sh - cd to the git repository root
 
+# ## graphql-scalarsをargument型に使った際の動作確認
+
 # argument type
 #   Apollo Studio, send wrong type - error
 #   Apollo Studio, send wrong format - error
